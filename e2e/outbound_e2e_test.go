af60c5550709833d1002760127c2897e674489ba
Liam <liam@evervault.com>
2025-09-12 15:28:55 +0100

    chore: port e2e to use assertions and requires instead of conditionals

--- e2e/outbound_e2e_test.go
+++ e2e/outbound_e2e_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/evervault/evervault-go/internal/testhelper"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -51,18 +52,7 @@ func TestE2EOutboundRelay(t *testing.T) {
 	//nolint:errcheck
 	_ = json.Unmarshal(body, &responseData)
 
-	if responseData["request"]["string"] != false {
-		t.Errorf("Expected false as response %t", responseData["request"]["string"])
-		return
-	}
-
-	if responseData["request"]["number"] != false {
-		t.Errorf("Expected false as response %t", responseData["request"]["number"])
-		return
-	}
-
-	if responseData["request"]["boolean"] != false {
-		t.Errorf("Expected false as response %t", responseData["request"]["boolean"])
-		return
-	}
+	assert.Falsef(t, responseData["request"]["string"], "Expected false as a response: %t", responseData["request"]["string"])
+	assert.Falsef(t, responseData["request"]["number"], "Expected false as a response: %t", responseData["request"]["number"])
+	assert.Falsef(t, responseData["request"]["boolean"], "Expected false as a response: %t", responseData["request"]["boolean"])
 }
